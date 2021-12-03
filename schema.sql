cb02f53f7082f386e4a6080a6fbfab1fcf99365a
rasheemg <93060074+rasheemg@users.noreply.github.com>
2021-12-02 21:42:45 -0500

    add some files

--- schema.sql
+++ schema.sql
@@ -35,4 +35,22 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dep_emp (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
 SELECT * FROM departments;
